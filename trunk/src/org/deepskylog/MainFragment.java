57f45a826266ace2dc51ffa5dab3c14a8bb731ad
david <david@585970db-1ada-4866-ad54-0b88e855cf08>
2014-02-16 15:24:50 +0000

    20140216 - Work in progress on auto login

--- trunk/src/org/deepskylog/MainFragment.java
+++ trunk/src/org/deepskylog/MainFragment.java
@@ -16,18 +16,10 @@ public class MainFragment extends Fragment {
 	private MainActivity mainActivity;
 	private View mainFragmentView;
 	
-	private TextView loggedperson_textview;
-	private Button login_button;
-	
-	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     	mainFragmentView=inflater.inflate(R.layout.mainfragment, container, false);    	
-       	loggedperson_textview=(TextView) mainFragmentView.findViewById(R.id.mainfragment_loggedperson_textview_id);
-       	login_button=(Button) mainFragmentView.findViewById(R.id.mainfragment_login_button_id);
-        login_button.setOnClickListener(new OnClickListener() { @Override public void onClick(View v) { login(); } });
-    	checkLoggedPerson();
-    	return mainFragmentView;
+     	return mainFragmentView;
 	}
 	@Override
 	public void onAttach(Activity activity) {
@@ -41,25 +33,6 @@ public class MainFragment extends Fragment {
 		super.onResume();
 	    mainActivity.actualFragment=this;
 	    mainActivity.actualFragmentName="mainFragment";
-	}
-	private void checkLoggedPerson() {
-		if(MainActivity.loggedPerson.equals("")) {
-			loggedperson_textview.setVisibility(View.GONE);
-			login_button.setVisibility(View.VISIBLE);
-		}
-		else {
-		
-			loggedperson_textview.setVisibility(View.VISIBLE);
-			login_button.setVisibility(View.GONE);
-		}
-	}
-	private void login() {
-		Toast.makeText(mainActivity, "DEVELOP: autologin being developed,  obsolete login",Toast.LENGTH_LONG).show();
-		//if(mainActivity.preferences.getString("loginName", "").equals("")) {
-		    //mainActivity.loginDialog.show(mainActivity.getFragmentManager(), "loginDialog");
-		    //mainActivity.actualFragmentName="loginDialog";
-			//mainActivity.goToFragment(mainActivity.ADD_TO_BACKSTACK);
-		//}
 	}
 }
 
