d0962dca4788fe5d0ca80ccd3af3ff680a3d1080
david <david@585970db-1ada-4866-ad54-0b88e855cf08>
2014-03-18 07:39:08 +0000

    20140318 - Code streamlining

--- trunk/src/org/deepskylog/Utils.java
+++ trunk/src/org/deepskylog/Utils.java
@@ -33,56 +33,46 @@ public class Utils {
 	        conn.connect();
 	        if(conn.getResponseCode()==200) {
 	        	inputStream=conn.getInputStream();
-	        	Intent intent=new Intent("org.deepskylog.online");
-	        	if(intent.getStringExtra("org.deepskylog.online")!=null) intent.removeExtra("org.deepskylog.online");
-	 			LocalBroadcastManager.getInstance(MainActivity.mainActivity).sendBroadcast(intent.putExtra("org.deepskylog.online", "online"));
+	        	LocalBroadcastManager.getInstance(MainActivity.mainActivity).sendBroadcast(new Intent("org.deepskylog.online").putExtra("org.deepskylog.online", "online"));
 		        return readIt(inputStream, 50000);
 	        }
 	        else {
-	        	Intent intent=new Intent("org.deepskylog.online");
-	        	if(intent.getStringExtra("org.deepskylog.online")!=null) intent.removeExtra("org.deepskylog.online");
-	 			LocalBroadcastManager.getInstance(MainActivity.mainActivity).sendBroadcast(intent.putExtra("org.deepskylog.online", "offline"));
+	        	LocalBroadcastManager.getInstance(MainActivity.mainActivity).sendBroadcast(new Intent("org.deepskylog.online").putExtra("org.deepskylog.online", "offline"));
 	        	return "<result>"+"Unavailable url: "+theUrl+"</result>";
 	        }
 	    }
- 	    catch (Exception e) { 
- 			Intent intent=new Intent("org.deepskylog.online");
- 			if(intent.getStringExtra("org.deepskylog.online")!=null) intent.removeExtra("org.deepskylog.online");
- 			LocalBroadcastManager.getInstance(MainActivity.mainActivity).sendBroadcast(intent.putExtra("org.deepskylog.online", "offline"));
+ 	    catch(Exception e) { 
+ 			LocalBroadcastManager.getInstance(MainActivity.mainActivity).sendBroadcast(new Intent("org.deepskylog.online").putExtra("org.deepskylog.online", "offline"));
         	return "<result>"+"Unavailable url: "+theUrl+"</result>"; 
         }
  	    finally {
- 	    	if (inputStream!=null) {
+ 	    	if(inputStream!=null) {
  	    		try { inputStream.close(); }
- 	    		catch(IOException e) { return "<result>"+"Command unavailable."+"</result>"; }
+ 	    		catch(IOException e) { }
 	        } 
  	    }
     }    
     
     private static String readIt(InputStream inputStream, int len) throws IOException, UnsupportedEncodingException {
-    	Reader reader=new InputStreamReader(inputStream);        
     	char[] buffer=new char[len];
-    	reader.read(buffer);
+    	(new InputStreamReader(inputStream)).read(buffer);
     	return new String(buffer).trim();
     }	
     
     public static String getTagContent(String result, String tag) throws Exception {
+  	   	if(result==null) throw new Exception("Utils.getTagContent: No string to analyse");
+  	   	if(tag==null) throw new Exception("Utils.getTagContent: No tag to analyse");
+  	   	if(tag.equals("")) throw new Exception("Utils.getTagContent: Empty tag to analyse");	   	
     	Integer datatag1=result.indexOf("<"+tag+">")+(tag.length()+2);
-  	   	if(datatag1==-1) {
-  	   		//Toast.makeText(MainActivity.mainActivity, "No opening tag "+tag+" in "+result,Toast.LENGTH_LONG).show();
-    		throw new Exception( "No opening tag "+tag+" in "+result);
-    	}
-		Integer datatag2=result.indexOf("</"+tag+">",datatag1);
-		if(datatag2==-1) {
-    		Toast.makeText(MainActivity.mainActivity, "No closing tag "+tag+" in "+result,Toast.LENGTH_LONG).show();
-    		throw new Exception( "No closing tag "+tag+" in "+result);
-      	}
+    	if(datatag1==-1) throw new Exception("Utils.getTagContent: No opening tag "+tag+" in "+result);
+    	Integer datatag2=result.indexOf("</"+tag+">",datatag1);
+		if(datatag2==-1) throw new Exception("Utils.getTagContent: No closing tag "+tag+" in "+result);
     	return result.substring(datatag1,datatag2);
     }
     
-    public static void onResultRaw(String result) {
+    public static void invokeClassMethodWithResult(String result) {
     	try { Class.forName(getTagContent(result,"onResultClass")).getMethod(getTagContent(result,"onResultMethod"), String.class).invoke(null,result); }
-    	catch(Exception e) { Toast.makeText(MainActivity.mainActivity,"Exception 2 in Utils "+result+e.getMessage().toString(), Toast.LENGTH_SHORT).show(); };
+    	catch(Exception e) { Toast.makeText(MainActivity.mainActivity,"Utils.invokeClassMethodWithResult: Exception 2, "+result+e.getMessage().toString(), Toast.LENGTH_SHORT).show(); };
     }
 
 
